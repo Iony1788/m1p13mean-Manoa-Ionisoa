151d9486f5f5c0cf2138e48d35f9d52e9eebbab9
Ionisoa <ionyso685@gmail.com>
2026-02-13 16:12:06 +0400

    edit cloudinary

--- backend/config/cloudinaryConfig.js
+++ backend/config/cloudinaryConfig.js
@@ -1,19 +1,19 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
-
+import dotenv from 'dotenv';
+dotenv.config(); 
 
 cloudinary.config({
-    cloud_name: 'dgn6teusy',
-    api_key: '842422141726933',
-    api_secret: 'r8EdpLE33qGtbk46UToCKmVwLL8'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: 'produits', 
+        folder: 'produits',
         allowed_formats: ['jpg', 'png', 'jpeg'],
         transformation: [{ width: 500, height: 500, crop: 'limit' }],
     },
