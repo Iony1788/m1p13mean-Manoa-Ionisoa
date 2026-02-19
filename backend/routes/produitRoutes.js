35003ef6ac368302bfb70988213052ca90053f62
Ionisoa <ionyso685@gmail.com>
2026-02-19 16:35:12 +0400

    add functions customer

--- backend/routes/produitRoutes.js
+++ backend/routes/produitRoutes.js
@@ -3,8 +3,21 @@ const router = express.Router();
 const produitController = require('../controllers/produitController');
 const parser = require('../config/cloudinaryConfig').parser;
 
+
+router.get('/categorie/:idCategorie', produitController.getListProduitWhitCategorie);
+
+
 router.get('/', produitController.getAllProduits);
+router.get('/:id', produitController.detailProduit);
+
+
+router.post('/:id/addCart', produitController.addCartProduit);
+router.delete('/:id/removeCart', produitController.removeCartProduit);
+router.put('/:id/updateCart', produitController.updateCartProduit);
+
 
 router.post('/add', parser.single('image'), produitController.addProduit);
+
+router.get('/listProduitBoutique/:idBoutique', produitController.getListProduitWhitBoutique);
 
 module.exports = router;
